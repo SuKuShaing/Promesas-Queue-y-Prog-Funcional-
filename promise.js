34b514f21e2c71addfdf970a31616f25d398a5ea
Sebastian <sebastian.sanhueza.t@usach.cl>
2024-08-23 13:52:43 -0400

    como funcionana las promesas

--- promise.js
+++ promise.js
@@ -1,5 +1,6 @@
 // https://youtu.be/VP61sxajsgo?si=BXGhty9goCiHIBfy
 
+// Cola a trabajar
 class Queue {
     #items = [];
 
@@ -15,3 +16,21 @@ class Queue {
         return this.#items.length === 0;
     }
 }
+
+// Como funcionan las promesas en JS (no tiene nada que ver con arriba es solamente para saber como funcionan las promesas)
+const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve('Promesa 1');
+    }, 4000);
+});
+
+// forma de ejecutar la promesa
+promise.then(res => console.log(res));
+
+// otra forma de ejecutar la promesa, función ifi (inmediatly invoked function expression) o función autoinvocada
+// se ejecuta inmediatamente cuando el interprete de JS llega a ella
+(async () => {
+    const res = await promise;
+    console.log(res);
+})();
+
